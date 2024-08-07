ff81d28f52a87c6331560c81f9ee594922a50c81
mitrikhoury <mitkhoury@gmail.com>
2024-08-07 10:46:02 +0300

    users_task

--- MySql_tasks/MySql_queries_task/users_task/users_database.sql
+++ MySql_tasks/MySql_queries_task/users_task/users_database.sql
@@ -5,17 +5,17 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema user_database
+-- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema user_database
+-- Schema mydb
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `user_database` DEFAULT CHARACTER SET utf8 ;
 USE `user_database` ;
 
 -- -----------------------------------------------------
--- Table `user_database`.`users`
+-- Table `mydb`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `user_database`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
